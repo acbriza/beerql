ebac5efcfcd90f1753946b7e6d55b5657eea4a2b
Tony Briza <acbriza@gmail.com>
2022-11-23 04:53:12 +0800

    beerenv: change action from tuple to vector

--- beerenv.py
+++ beerenv.py
@@ -1,5 +1,6 @@
 import sys
 import random
+import math
 from gym import Env, spaces
 
 from data import *
@@ -17,13 +18,21 @@ class BeerGameEnv(Env):
              spaces.Discrete(9, start=1)) 
         )        
         
-        # policy (y) values in [0,1,2,3]
-        self.action_space = spaces.Tuple(
-            (spaces.Discrete(4, start=0), 
-             spaces.Discrete(4, start=0), 
-             spaces.Discrete(4, start=0), 
-             spaces.Discrete(4, start=0)) 
-        )           
+        # number of actions is 4^4
+        self.action_space = spaces.Discrete(256) 
+
+        # comment out vector form of actions for the mean time
+        # # policy (y) values in [0,1,2,3]
+        # self.action_space = spaces.Tuple(
+        #     (spaces.Discrete(4, start=0), 
+        #      spaces.Discrete(4, start=0), 
+        #      spaces.Discrete(4, start=0), 
+        #      spaces.Discrete(4, start=0)) 
+        # )
+
+        # # number of actions is 4^4
+        # na = len(self.action_space)
+        # self.nA = pow(na,na)
 
         self.sc = SupplyChain(data=data[MAIN], policy=RLCha08)
 
@@ -37,7 +46,7 @@ class BeerGameEnv(Env):
 
     def step(self, action):
         assert self.action_space.contains(action)
-        obs, reward, done, info = sc.step(action) 
+        obs, reward, done, info = self.sc.rl_env_step(action) 
 
         self.render(action, reward, obs)
         return obs, reward, done, info
@@ -54,7 +63,7 @@ class BeerGameEnv(Env):
 
     def render(self, action, reward, obs):
         print("=============================================================================")
-        print(f"t: {self.sc.t};  Action: {action}")
+        print(f"t: {self.sc.t};  Action: {self.sc.action_space_tuples[action]}")
         print(f"Reward: {reward}; Total rewards: {self.sc.total_cost}")
         print(f"S': {obs}")
 
