32458ac86512b47654a0992d5a51a39d54025f0b
Tony Briza <acbriza@gmail.com>
2022-11-23 10:45:43 +0800

    add simulation adn rl_train functions

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-import pandas as pd
-import numpy as np
-#import matplotlib.pyplot as plt
 from data import *
 from agent import SCAgent, SupplyChain
-sc = SupplyChain(data=data[MAIN], policy=RL)
-report = sc.run(5)
 
-df = pd.DataFrame.from_dict(report, orient="index").stack().to_frame()
-# to break out the lists into columns
-df = pd.DataFrame(df[0].values.tolist(), index=df.index)
-print(df.T)
+import numpy as np
+import pandas as pd
+
+from beerenv import BeerGameEnv
+from ql import q_learning
+
+def simulation():
+    sc = SupplyChain(data=data[MAIN], policy=RL)
+    report = sc.run(5)
+
+    df = pd.DataFrame.from_dict(report, orient="index").stack().to_frame()
+    # to break out the lists into columns
+    df = pd.DataFrame(df[0].values.tolist(), index=df.index)
+    print(df.T)
+
+def rl_train():
+    env = BeerGameEnv()
+    Q, stats = q_learning(env, num_episodes=2000, alpha=0.17)
+    return Q, stats
+
+
+Q, stats = rl_train()
